cd2658b5490ba91370c6c5979c58c7a893c12fcb
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-06-08 21:33:48 +0200

    Added modal

--- script.js
+++ script.js
@@ -3,40 +3,69 @@ const btnCardLayout = document.getElementById('btnCardLayout')
 const btnListLayout = document.getElementById('btnListLayout')
 const gridContainer = document.getElementById('gridContainer')
 const gridItem = document.querySelectorAll('.grid-card')
-const cardText= document.querySelectorAll('.card-text')
-const authorText= document.querySelectorAll('.author')
-const bookTitle= document.querySelectorAll('.book-title')
-const pagesNumber= document.querySelectorAll('.pages-number')
-const cardButtons= document.querySelectorAll('.card-buttons')
+const cardText = document.querySelectorAll('.card-text')
+const authorText = document.querySelectorAll('.author')
+const bookTitle = document.querySelectorAll('.book-title')
+const pagesNumber = document.querySelectorAll('.pages-number')
+const cardButtons = document.querySelectorAll('.card-buttons')
+const modalBtn = document.getElementById('modalBtn')
+const closeModalBtn = document.getElementById('closeBtn')
+const overlay = document.getElementById('overlay')
+const modal = document.getElementById('modal')
 
 
 let myLibrary = [];
 
 function Book(title, author, pages, status) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
 }
 
 function addBookToLibrary() {
-  // do stuff here
+    // do stuff here
+}
+
+
+modalBtn.addEventListener('click', () => {
+    openModal(modal)
+})
+
+closeModalBtn.addEventListener('click', () => {
+    closeModal(modal)
+})
+
+overlay.addEventListener('click', () => {
+    closeModal(modal)
+})
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
 }
 
 readBtn.forEach(button => {
-    button.addEventListener('click', () =>{
-        if(button.classList.contains('btn-read')){
+    button.addEventListener('click', () => {
+        if (button.classList.contains('btn-read')) {
             button.classList.remove('btn-read')
-            button.textContent="Unread";
-        }else{
+            button.textContent = "Unread";
+        } else {
             button.classList.add('btn-read')
-            button.textContent="Read";
+            button.textContent = "Read";
         }
     })
 })
 
 btnCardLayout.addEventListener('click', () => {
-    if(!btnCardLayout.classList.contains('white-border')){
+    if (!btnCardLayout.classList.contains('white-border')) {
         btnCardLayout.classList.add('white-border')
         btnListLayout.classList.remove('white-border')
         gridContainer.classList.remove('grid-container-list')
@@ -64,7 +93,7 @@ btnCardLayout.addEventListener('click', () => {
 })
 
 btnListLayout.addEventListener('click', () => {
-    if(!btnListLayout.classList.contains('white-border')){
+    if (!btnListLayout.classList.contains('white-border')) {
         btnListLayout.classList.add('white-border')
         btnCardLayout.classList.remove('white-border')
         gridContainer.classList.remove('grid-container')
