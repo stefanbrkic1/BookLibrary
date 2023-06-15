96c90eeb6931032b38f449d116f068761c926e80
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-06-15 18:34:17 +0200

    Added remove and reading status functionality

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ const overlay = document.getElementById('overlay')
 const modal = document.getElementById('modal')
 const addBtn = document.getElementById('addBtn')
 const form = document.getElementById('form')
-const readingStatus = document.getElementsByName('readingStatus')
-const radioReadInput = document.getElementById('radioRead')
-const radioUnreadInput = document.getElementById('radioUnread')
 
 window.onload = function(){
     displayBooks();
@@ -43,25 +40,49 @@ let myLibrary = [
     {
         title: 'Atomic Habbits',
         author: 'James Clear',
-        pages: '245',
+        pages: '250',
         status: 'Read'
     },
     {
         title: 'The 48 Laws of Power',
         author: 'Robert Greene',
-        pages: '221',
+        pages: '452',
         status: 'Read'
     },
     {
         title: 'Meditations',
         author: 'Marcus Aurelius',
-        pages: '180',
+        pages: '410',
         status: 'Read'
     },
     {
         title: 'The Idiot',
         author: 'Fyodor Dostoyevski',
-        pages: '357',
+        pages: '768',
+        status: 'Read'
+    },
+    {
+        title: 'The Art Of War',
+        author: 'Sun Tzu',
+        pages: '260',
+        status: 'Read'
+    },
+    {
+        title: 'Republic',
+        author: 'Plato',
+        pages: '692',
+        status: 'Read'
+    },
+    {
+        title: 'Tao Te Ching',
+        author: 'Laozi',
+        pages: '84',
+        status: 'Read'
+    },
+    {
+        title: '12 Rules for Life',
+        author: 'Jordan Peterson',
+        pages: '409',
         status: 'Read'
     },
 ];
@@ -74,48 +95,104 @@ function Book(title, author, pages, status) {
 }
 
 function displayBooks(){
-    gridContainer.innerHTML= '';
-    if(btnCardLayout.classList.contains('white-border')){
-        myLibrary.forEach(book => {
-            const div = document.createElement('div');
-            div.className = 'grid-card grid-item';
-            div.innerHTML =    `<div class="card-text card-text-script">
-                <div class="book-title-script book-title">${book.title}</div>
-                <div class="author-script author">By ${book.author}</div>
-                <div class="pages-number">Pages: ${book.pages}</div>
-            </div>
-            <div class="card-buttons-script card-buttons-container card-buttons">
-                <button type="button" class="read-btn btn-read">${book.status}</button>
-                <button type="button" class="remove-btn">Remove</button>
-            </div>`;
-    
-        gridContainer.appendChild(div);
-        })
+    gridContainer.innerHTML=''
+ myLibrary.forEach((book, index) => {
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('grid-item-script', 'grid-item');
+
+    const cardTextDiv = document.createElement('div');
+    cardTextDiv.classList.add('card-text-script', 'card-text');
+
+    const cardBookTitleDiv = document.createElement('div');
+    cardBookTitleDiv.classList.add('book-title-script', 'book-title');
+    cardBookTitleDiv.textContent = book.title; // Set book title
+
+    const cardBookAuthorDiv = document.createElement('div');
+    cardBookAuthorDiv.classList.add('author-script', 'author');
+    cardBookAuthorDiv.textContent = book.author; // Set book author
+
+    const cardBookPagesDiv = document.createElement('div');
+    cardBookPagesDiv.classList.add('pages-number-script', 'pages-number');
+    cardBookPagesDiv.textContent = book.pages; // Set book pages
+
+    const cardButtonsDiv = document.createElement('div');
+    cardButtonsDiv.classList.add('card-buttons-script', 'card-buttons');
+
+    const cardStatusBtn = document.createElement('button');
+    cardStatusBtn.classList.add('btn-status');
+    cardStatusBtn.dataset.index = index;
+    if(book.status === 'Read'){
+        cardStatusBtn.textContent = 'Read'
+        cardStatusBtn.classList.add('btn-read')
     }
     else{
-        myLibrary.forEach(book => {
-            const div = document.createElement('div');
-            div.className = 'grid-card grid-item-list';
-            div.innerHTML =    `<div class="card-text-script card-text-list">
-                <div class="book-title-script book-title-list">${book.title}</div>
-                <div class="author-script author-list">By ${book.author}</div>
-                <div class="pages-number author-list">Pages: ${book.pages}</div>
-            </div>
-            <div class="card-buttons-script card-buttons-container card-buttons-list">
-                <button type="button" class="read-btn btn-read">${book.status}</button>
-                <button type="button" class="remove-btn">Remove</button>
-            </div>`;
-    
-        gridContainer.appendChild(div);
-        })
+        cardStatusBtn.textContent = 'Unread'
     }
-    gridItem = document.querySelectorAll('.grid-card')
+
+    const cardRemoveBtn = document.createElement('button');
+    cardRemoveBtn.classList.add('remove-btn');
+    cardRemoveBtn.textContent = 'Remove'
+    cardRemoveBtn.dataset.index = index;
+
+    gridContainer.appendChild(cardDiv)
+    cardDiv.appendChild(cardTextDiv)
+    cardDiv.appendChild(cardButtonsDiv)
+    cardTextDiv.appendChild(cardBookTitleDiv)
+    cardTextDiv.appendChild(cardBookAuthorDiv)
+    cardTextDiv.appendChild(cardBookPagesDiv)
+    cardButtonsDiv.appendChild(cardStatusBtn)
+    cardButtonsDiv.appendChild(cardRemoveBtn)
+
+    if(btnListLayout.classList.contains('white-border')){
+        cardDiv.classList.remove('grid-item');
+        cardDiv.classList.add('grid-item-list');
+        cardTextDiv.classList.remove('card-text');
+        cardTextDiv.classList.add('card-text-list');
+        cardBookTitleDiv.classList.remove('book-title');
+        cardBookTitleDiv.classList.add('book-title-list');
+        cardBookAuthorDiv.classList.remove('author');
+        cardBookAuthorDiv.classList.add('author-list');
+        cardBookPagesDiv.classList.remove('pages-number');
+        cardBookPagesDiv.classList.add('author-list');
+        cardButtonsDiv.classList.remove('card-buttons');
+        cardButtonsDiv.classList.add('card-buttons-list');
+    }
+
+    statusButtons = document.querySelectorAll('.btn-status')
+    gridItem = document.querySelectorAll('.grid-item-script')
     cardText = document.querySelectorAll('.card-text-script')
     authorText = document.querySelectorAll('.author-script')
     bookTitle = document.querySelectorAll('.book-title-script')
-    pagesNumber = document.querySelectorAll('.pages-number')
+    pagesNumber = document.querySelectorAll('.pages-number-script')
     cardButtons = document.querySelectorAll('.card-buttons-script')
+ })  
 }
+
+gridContainer.addEventListener('click', function(event) {
+    const target = event.target;
+    
+    // Check if the clicked element has the class 'btn-status'
+    if (target.classList.contains('btn-status')) {
+      const index = target.dataset.index;
+      // Access the book object using the index and update the status
+      const book = myLibrary[index];
+      if (book.status === 'Read') {
+        book.status = 'Unread';
+      } else {
+        book.status = 'Read';
+      }
+      displayBooks(); // Update the display to reflect the status change
+    }
+    
+    // Check if the clicked element has the class 'remove-btn'
+    if (target.classList.contains('remove-btn')) {
+      const index = target.dataset.index;
+      // Remove the book from the myLibrary array using the index
+      myLibrary.splice(index, 1);
+      displayBooks(); // Update the display after removing the book
+    }
+  });
+  
 
 modalBtn.addEventListener('click', () => {
     openModal(modal)
@@ -151,7 +228,6 @@ btnCardLayout.addEventListener('click', () => {
             card.classList.remove('grid-item-list')
             card.classList.add('grid-item')
         })
-        console.log(cardText    )
         cardText.forEach(card => {
             card.classList.remove('card-text-list')
             card.classList.add('card-text')
@@ -202,3 +278,5 @@ btnListLayout.addEventListener('click', () => {
         })
     }
 })
+
+
